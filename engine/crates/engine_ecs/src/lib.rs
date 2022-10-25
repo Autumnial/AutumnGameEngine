7875ea6443b187c74f2e3d64dece8e08f3b84418
Autumn <celesteialcutie@gmail.com>
2022-10-25 17:00:28 +0200

    project setup - file organisation - simple logging - start at ECS

---
+++ engine/crates/engine_ecs/src/lib.rs
@@ -0,0 +1,96 @@
+use engine_logger::*;
+use std::cell::{RefCell, RefMut};
+
+//todo better way of querying entities
+//todo deleting entities
+//todo removing components
+
+
+
+pub struct World{
+    entities: usize,
+    components: Vec<Box<dyn ComponentVec>>
+}
+
+impl World {
+    pub fn new() -> Self{
+        Self { entities: 0, components: Vec::new() }
+    }
+
+    pub fn update(&mut self){
+        //do stuff
+    }
+
+    pub fn add_entity(&mut self) -> usize{
+        
+        for component_vec in self.components.iter_mut(){
+            component_vec.push_none()
+        }
+        let entity_id = self.entities;
+        self.entities += 1;
+        entity_id
+    }
+
+    pub fn add_component_to_entity<ComponentType: 'static>(
+        &mut self,
+        entity: usize,
+        component: ComponentType
+    ){
+        for component_vec in self.components.iter_mut(){
+            if let Some(component_vec) = component_vec
+                .as_any_mut()
+                .downcast_mut::<RefCell<Vec<Option<ComponentType>>>>()
+                {
+                    component_vec.get_mut()[entity] = Some(component);
+                    return;
+            }
+        }
+
+        let mut new_component_vec: Vec<Option<ComponentType>> = Vec::with_capacity(self.entities);
+
+        for _ in 0..self.entities{
+            new_component_vec.push(None)
+        }
+
+        new_component_vec[entity] = Some(component);
+        self.components.push(Box::new(RefCell::new(new_component_vec)));
+    }
+
+    pub fn borrow_component_vec<ComponentType: 'static>(&self) -> RefMut<Vec<Option<ComponentType>>>{
+
+        for component_vec in self.components.iter(){
+            if let Some(component_vec) = component_vec
+            .as_any()
+            .downcast_ref::<RefCell<Vec<Option<ComponentType>>>>(){
+                return component_vec.borrow_mut();
+            }
+        }
+        error!("Component vector does not exist");
+
+    }
+
+
+}
+
+trait ComponentVec{
+    fn as_any(&self) -> &dyn std::any::Any;
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
+    fn push_none(&mut self);
+}
+
+
+impl<T: 'static> ComponentVec for RefCell<Vec<Option<T>>>{
+
+    fn as_any(&self) -> &dyn std::any::Any {
+        self as &dyn std::any::Any
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self as &mut dyn std::any::Any
+    }
+
+    fn push_none(&mut self) {
+        self.get_mut().push(None);
+    }
+}
+
---
+++ engine/crates/engine_internal/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod prelude;
+
+pub mod ecs{
+    pub use engine_ecs::*;
+} 
+
+pub mod logger{
+    pub use engine_logger::*;
+} 
